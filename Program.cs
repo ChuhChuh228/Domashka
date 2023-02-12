1620df32edd3ef9dbbd70c42fc9f5817ed0e1532
Chuch <mwolf2380@gmail.com>
2023-02-12 12:50:18 +0200

    А зачем я удолил усе

---
+++ Program.cs
@@ -0,0 +1,8 @@
+//var students = new List<Student>()
+//{
+//    new Student{Name = "Vasya", Group = "SBU228", AvgMark = 10.5},
+//    new Student{Name = "Srehii", Group = "SBU228", AvgMark = 8.7},
+//    new Student{Name = "Gleb", Group = "RZHD538", AvgMark = 9.1},
+//    new Student{Name = "Petrii", Group = "RZHD538", AvgMark = 10.9},
+//};
+
